eb8c938073ef83111a3a658b45134507be994d5a
TrackerSL <157577084+TrackerSL@users.noreply.github.com>
2024-01-26 09:54:23 +0530

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
+  ownerID: ["832927766012756039"],
   botInvite: "",
   supportServer: "",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
